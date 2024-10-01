01b819cc53567f2e6fa1e16a91f1a31389f215aa
Alex Snow <acsnow99@gmail.com>
2024-10-01 04:43:56 -0800

    Tests update functionality

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../service');
 
+const adminUser = { name: 'admin', email: 'admin@test.com', password: 'ad', roles: [{ role: 'admin' }] };
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
@@ -11,12 +12,41 @@ beforeAll(async () => {
   expect(testUserAuthToken).toBeTruthy();
 });
 
+beforeEach(async () => {
+  await request(app).delete('/api/auth').set('Authorization', 'Bearer ' + testUserAuthToken);
+});
+
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
   expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  testUserAuthToken = loginRes.body.token;
 
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
   expect(password).toBeTruthy();
 });
+
+test('register', async () => {
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  expect(registerRes.status).toBe(200);
+  expect(registerRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  testUserAuthToken = registerRes.body.token;
+
+  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(registerRes.body.user).toMatchObject(user);
+  expect(password).toBeTruthy();
+});
+
+test('update', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  testUserAuthToken = loginRes.body.token;
+
+  const newEmailString = 'notAnEmail@gmail.com';
+  const newUser = { email: newEmailString, password: 'notapassword' };
+  const updateRes = await request(app).put(`/api/auth/${loginRes.body.user.id}`).set('Authorization', 'Bearer ' + testUserAuthToken).set('Content-Type', 'application/json').send(newUser);
+  expect(updateRes.status).toBe(200);
+
+  console.log("Users", testUser, updateRes.body)
+  expect(updateRes.body.email).toBe(newEmailString);
+})
