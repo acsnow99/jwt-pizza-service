e98de26b20bf91bd1b6963bd57d1c0e012978c0e
Alex Snow <acsnow99@gmail.com>
2024-10-01 07:54:31 -0800

    Removed unused var

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -6,7 +6,6 @@ const originalPassword = 'passwordagain';
 const adminUserRegister = { name: 'admin', email: 'a@gmail.com', password: 'passwordagain', roles: [{ role: Role.Admin }] }
 const testMenuItem = { title: 'my pizza', description: 'nothing', image: 'pizza9.png', price: 0.001 };
 let testUserAuthToken;
-let testUserId;
 
 beforeAll(async () => {
     adminUserRegister.email = Math.random().toString(36).substring(2, 12) + '@test.com';
@@ -20,7 +19,6 @@ beforeAll(async () => {
     expect(loginRes.status).toBe(200);
     
     testUserAuthToken = loginRes.body.token;
-    testUserId = addResult.id;
 });
 
 test('add an item to menu', async () => {
