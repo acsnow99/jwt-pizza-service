20237b6336fe44d448e7e4494e0ad54f51566c89
Alex Snow <acsnow99@gmail.com>
2024-10-01 07:42:53 -0800

    Deleting store works

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -21,10 +21,10 @@ beforeAll(async () => {
     expect(loginRes.status).toBe(200);
     
     testUserAuthToken = loginRes.body.token;
-    testUserId = loginRes.body.id;
+    testUserId = addResult.id;
     
     testFranchise.name = Math.random().toString(36).substring(2, 12);
-    testFranchise.admins[0].email = adminUserRegister.email;
+    testFranchise.admins[0].email = addResult.email;
 });
 
 test('create franchise', async () => {
@@ -37,19 +37,9 @@ test('create franchise', async () => {
 test('get franchises', async () => {
     const getRes = await request(app).get('/api/franchise');
     expect(getRes.status).toBe(200);
-    console.log("Got franchises:", getRes.body);
     const names = [];
     getRes.body.forEach((fran) => names.push(fran.name));
-    expect(names.includes(testFranchise.name));
-});
-
-test('get a user\'s franchises', async () => {
-    const getRes = await request(app).get('/api/franchise/' + testUserId).set('Authorization', 'Bearer ' + testUserAuthToken);
-    expect(getRes.status).toBe(200);
-    console.log("Got franchises:", getRes.body);
-    const names = [];
-    getRes.body.forEach((fran) => names.push(fran.name));
-    expect(names.includes(testFranchise.name));
+    expect(names.includes(testFranchise.name)).toBeTruthy();
 });
 
 test('create store', async () => {
@@ -58,6 +48,23 @@ test('create store', async () => {
     expect(createRes.status).toBe(200);
     expect(createRes.body.name).toBe(testStore.name);
     testStore.id = createRes.body.id;
+});
+
+test('get a user\'s franchises', async () => {
+    const getRes = await request(app).get('/api/franchise/' + testUserId).set('Authorization', 'Bearer ' + testUserAuthToken);
+    expect(getRes.status).toBe(200);
+    console.log("Got franchises:", getRes.body);
+    const names = [];
+    getRes.body.forEach((fran) => names.push(fran.name));
+    expect(names.includes(testFranchise.name)).toBeTruthy();
+});
+
+test('delete a store', async () => {
+    const deleteRes = await request(app).delete(`/api/franchise/${testFranchise.id}/store/${testStore.id}`).set('Authorization', 'Bearer ' + testUserAuthToken);
+    expect(deleteRes.status).toBe(200);
+    const getRes = await request(app).get('/api/franchise/' + testUserId).set('Authorization', 'Bearer ' + testUserAuthToken);
+    console.log("Franchises again:", getRes.body);
+    expect(getRes.body[0].stores).toStrictEqual([]);
 });
 
 test('delete a franchise', async () => {
