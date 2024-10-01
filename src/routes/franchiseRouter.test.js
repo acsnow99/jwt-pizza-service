416805de251932ccc1bbde728f3e00a836da66c9
Alex Snow <acsnow99@gmail.com>
2024-10-01 05:37:19 -0800

    Getting a user's franchises works

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -4,10 +4,12 @@ const app = require('../service');
 const adminUser = { email: 'a@gmail.com', password: 'passwordagain'}
 const testFranchise = {name: 'pizzaTest', admins: [{email: 'admin@admin.admin'}]};
 let testUserAuthToken;
+let testUserId;
 
 beforeAll(async () => {
     const loginRes = await request(app).put('/api/auth').send(adminUser);
     testUserAuthToken = loginRes.body.token;
+    testUserId = loginRes.body.user.id;
     testFranchise.name = Math.random().toString(36).substring(2, 12);
 });
 
@@ -19,6 +21,15 @@ test('create franchise', async () => {
 
 test('get franchises', async () => {
     const getRes = await request(app).get('/api/franchise');
+    expect(getRes.status).toBe(200);
+    console.log("Got franchises:", getRes.body);
+    const names = [];
+    getRes.body.forEach((fran) => names.push(fran.name));
+    expect(names.includes(testFranchise.name));
+});
+
+test('get a user\'s franchises', async () => {
+    const getRes = await request(app).get('/api/franchise/' + testUserId).set('Authorization', 'Bearer ' + testUserAuthToken);
     expect(getRes.status).toBe(200);
     console.log("Got franchises:", getRes.body);
     const names = [];
